79eed8f81a2b35335a157f0e4dd266fb88a7d912
Caspuhh <95343459+casparml@users.noreply.github.com>
2024-05-15 22:34:50 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,28 +23,6 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-const modalViews = document.querySelectorAll('.help__modal');
-const modalBtns = document.querySelectorAll('.help__button');
-const modalCloses = document.querySelectorAll('.help__modal-close');
-
-let modal = function(modalClick) {
-	modalViews[modalClick].classList.add('active-modal');
-};
-
-modalBtns.forEach((modalBtn, i) => {
-	modalBtn.addEventListener('click', () => {
-		modal(i);
-	});
-});
-
-modalCloses.forEach(modalClose => {
-	modalClose.addEventListener('click', () => {
-		modalViews.forEach(modalView => {
-			modalView.classList.remove('active-modal');
-		});
-	});
-});
-
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
@@ -108,61 +86,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
 });
-
-// Regex Search
-$(document).ready(function () {
-	var wordsArray = wordsData;
-	
-	$("[name='theme']").keyup(function () {
-		var words = $(this).val().replace(/_/g, '.');
-		
-		console.log(words);
-
-		try {
-			var searchWord = words.toLowerCase()
-			var regex = new RegExp(searchWord);
-
-			$("#result").html('');
-
-			for (var index = 0; index < wordsArray.length; index++) {
-				var value = wordsArray[index];
-				var eachWord = value.toLowerCase();
-
-				if (regex.test(eachWord)) {
-					var i;
-
-					if (searchWord.length != eachWord.length) {
-						continue;
-					};
-
-					for (i = 0; i < searchWord.length; i++) {
-						if (searchWord[i] == '.') {
-							continue;
-						};
-
-						if (searchWord[i] != eachWord[i]) {
-							break;
-						};
-					};
-
-					if (i == searchWord.length) {
-						$("#result").append('<p title="Click to copy to clipboard">' + '<a class="a_word" href="#form">' + value + '</a>' + '<br>');
-					};
-				};
-			};
-		} catch (error) {
-			console.log(error);
-		};
-	});
-});
-
-var count = document.getElementById("count");
-var input = document.getElementById("input");
-
-input.addEventListener("input", function(event){
-	count.innerHTML = event.target.value.length;
-});
-
 
 // Email
 function emailEnter() {
