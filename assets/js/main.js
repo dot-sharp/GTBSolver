440f84aa0577b676336a812486d39ac921e40301
casparml <95343459+casparml@users.noreply.github.com>
2024-07-03 14:03:48 +0200

    couple small fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,28 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+const modalViews = document.querySelectorAll('.help__modal');
+const modalBtns = document.querySelectorAll('.help__button');
+const modalCloses = document.querySelectorAll('.help__modal-close');
+
+let modal = function(modalClick) {
+	modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		modal(i);
+	});
+});
+
+modalCloses.forEach(modalClose => {
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach(modalView => {
+			modalView.classList.remove('active-modal');
+		});
+	});
+});
+
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
@@ -85,6 +107,71 @@ themeButton.addEventListener('click', () => {
     themeButton.classList.toggle(iconTheme);
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// Regex Search
+$(document).ready(function () {
+	var wordsArray = wordsData;
+	
+	$("[name='theme']").keyup(function () {
+		var words = $(this).val().replace(/_/g, '.');
+		
+		console.log(words);
+
+		try {
+			var searchWord = words.toLowerCase()
+			var regex = new RegExp(searchWord);
+
+			$("#result").html('');
+
+			for (var index = 0; index < wordsArray.length; index++) {
+				var value = wordsArray[index];
+				var eachWord = value.toLowerCase();
+			
+				if (regex.test(eachWord)) {
+					var i;
+
+					if (searchWord.length != eachWord.length) {
+						continue;
+					};
+
+					for (i = 0; i < searchWord.length; i++) {
+						if (searchWord[i] == '.') {
+							continue;
+						};
+
+						if (searchWord[i] != eachWord[i]) {
+							break;
+						};
+					};
+					
+					if (i == searchWord.length) {
+						$("#result").append('<p title="Select to copy to clipboard">' + '<a class="a_word" id="copyButton" href="#form">' + value + '</a>' + '<br>');
+
+						document.getElementById("copyButton").addEventListener("click", function() {
+  						var copyText = document.getElementById("copyText").value;
+						navigator.clipboard.writeText(copyText)
+						.then(function() {
+							console.log("Text copied to clipboard: " + copyText);
+						})
+						.catch(function(error) {
+							console.log("Failed to copy text: " + error);
+						});
+					});
+					};
+				};
+			};
+		} catch (error) {
+			console.log(error);
+		};
+	});
+});
+
+var count = document.getElementById("count");
+var input = document.getElementById("input");
+
+input.addEventListener("input", function(event){
+	count.innerHTML = event.target.value.length;
 });
 
 // Email
