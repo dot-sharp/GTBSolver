642fa38cd2fb74b6205d62d309c503892040e335
casparml <95343459+casparml@users.noreply.github.com>
2024-07-09 14:15:17 +0200

    debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 const sections = document.querySelectorAll('section[id]');
 
-/* function scrollActive() {
+function scrollActive() {
     const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
@@ -33,13 +33,13 @@ const sections = document.querySelectorAll('section[id]');
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id');
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        /* if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
         } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
-        };
+        }; */
     });
-}; */
+}; 
 
 window.addEventListener('scroll', scrollActive);
 
