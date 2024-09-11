f0c4dfa32e781e469b1b9fac7e05998e72b089c3
casparml <95343459+casparml@users.noreply.github.com>
2024-09-11 20:23:14 +0200

    dynamic title and favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,3 +192,26 @@ function youtubeLeave() {
 	document.getElementById("youtube__icon").style.setProperty('color', '#576ee0');
 	document.getElementById("youtube__link").style.setProperty('color', '#75798a');
 };
+
+
+// Dynamic favicon and title
+window.onload = function () {
+	const favicon = document.getElementById('favicon')
+	const pageTitle = document.title
+	const attentionMessage = 'Come back'
+  
+	document.addEventListener('visibilitychange', function (e) {
+	  const isPageActive = !document.hidden
+	  toggle(isPageActive)
+	})
+  
+	function toggle(isPageActive) {
+	  if (isPageActive) {
+		document.title = pageTitle
+		favicon.href = './img/logo.png'
+	  } else {
+		document.title = attentionMessage
+		favicon.href = './img/logo-dark.png'
+	  }
+	}
+  }
