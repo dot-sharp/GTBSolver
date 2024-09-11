4b354baeb603f2e5af68d46c97fa5f396d58d9fa
casparml <95343459+casparml@users.noreply.github.com>
2024-09-12 00:18:44 +0200

    hotfix

--- assets/js/dynamicFavicon.js
+++ assets/js/dynamicFavicon.js
@@ -1,8 +1,6 @@
-// Dynamic favicon and title
+// Dynamic favicon
 window.onload = function () {
 	const favicon = document.getElementById('favicon')
-	const pageTitle = document.title
-	const attentionMessage = 'GTB Solver'
   
 	document.addEventListener('visibilitychange', function (e) {
 	  const isPageActive = !document.hidden
@@ -11,10 +9,8 @@ window.onload = function () {
   
 	function toggle(isPageActive) {
 	  if (isPageActive) {
-		document.title = pageTitle
 		favicon.href = './assets/images/logo.png'
 	  } else {
-		document.title = attentionMessage
 		favicon.href = './assets/images/logo-away.png'
 	  }
 	}
