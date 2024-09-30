a98149b1588724b0385f1f467c679a1258c47780
Casparml <casparmlandman@gmail.com>
2024-09-30 11:00:54 +0200

    debug

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
-// // Window Height fix //
-// function resetHeight(){
-//     // reset the body height to that of the inner browser
-//     document.body.style.height = window.innerHeight + "px";
-// }
-// // reset the height whenever the window's resized
-// window.addEventListener("resize", resetHeight);
-// // called to initially set the height.
-// resetHeight();
+// Window Height fix //
+function resetHeight(){
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
 
 
 // Dynamic favicon //
