691e9371ac103f904068a9658392ef8b8824bd24
Casparml <casparmlandman@gmail.com>
2024-09-30 11:34:31 +0200

    hotfix

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,24 +54,6 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-const sections = document.querySelectorAll('section[id]');
-
-function scrollActive() {
-    const scrollY = window.scrollY;
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute('id');
-
-		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
-        };
-    });
-}; 
-
 window.addEventListener('scroll', scrollActive);
 
 function scrollHeader() {
